7857ef380449982de5edf6ba0fc406e65486d881
Melissa O'Neill <oneill@acm.org>
2020-06-28 18:44:04 -0700

    Add more ROM3 calls and update examples.

--- rom_example.c
+++ rom_example.c
@@ -11,10 +11,41 @@ int main()
 {
     int i;
     short free_mem;
+    char key;
+    char *cursor_pos;
+    char *cp;
+//  zxn_rom3_cls();
+    zxn_rom3_chan_open(2);
     printf("Hello World\n");
+    zxn_rom3_chan_open(1);
+    zxn_rom3_pr_string("Hello World! ",12);
+    zxn_rom3_chan_open(2);
     for (i = 0; i < 256; ++i)
 	zxn_rom3_plot_sub(i,i/2);
     zxn_rom3_beeper(0x0085, 0x066a);
+
+    zxn_rom3_chan_open(1);
+    zxn_rom3_set_min();
+    cursor_pos = (char*) zxn_rom3_bc_spaces(1);
+    *cursor_pos = '\r';
+    *((char*) 0x5C71) |= 0xa0;        // Set bits 7 and 5 of FLAGX
+//  None of these lines are needed/helpful.
+//  *((char*) 0x5c3c) |= 8;           // Set bit 3 of TV_FLAG
+//  *((char*) 0x5C71) &= 0xbf;        // Clear bit 6 of FLAGX (not needed here)
+//  *((char*) 0x5c3b) |= 0x0c;        // Set bits 2 and 3 of FLAGS (N/A)
+    *((char**) 0x5C5B) = cursor_pos;  // Set K_CUR to cursor position.
+    key = zxn_rom3_wait_key();
+    zxn_rom3_chan_open(2);
+    printf("You pressed '%c'\n", key);
+    zxn_rom3_chan_open(1);
+    zxn_rom3_pr_string(" | Prompt> ",11);
+    zxn_rom3_editor();
+    zxn_rom3_chan_open(2);
+    cp = cursor_pos;
+    while (*cp != '\r')
+	++cp;
+    *cp = '\0';
+    printf("You entered: '%s'\n", cursor_pos);
 
     // This value is naccurate because the stack is moved above RAMTOP
     free_mem = 65535 - (unsigned short)zxn_rom3_test_room(0);
--- zxn_rom3call.h
+++ zxn_rom3call.h
@@ -19,6 +19,10 @@ extern long zxn_rom3call_long(void* cont, short callno, short hlprime,
 
 #define ZXN_ROM3_BC_SPACES  0x0030    // allocate workspace
 
+#define zxn_rom3_bc_spaces(size)					\
+    ((void*) zxn_rom3call(zxn_p3cont_de, ZXN_ROM3_BC_SPACES, 0,		\
+			   0, size, 0, 0, 0))
+
 #define ZXN_ROM3_BEEPER     0x03b5    // make a sound using the beeper
 
 #define zxn_rom3_beeper(freqtime, tstates)			\
@@ -29,13 +33,13 @@ extern long zxn_rom3call_long(void* cont, short callno, short hlprime,
 
 #define zxn_rom3_sv_bytes(type, length, start)		\
     zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_SV_BYTES, 0,	\
-                 (type << 8), 0, length, 0, start)
+                 ((type) << 8), 0, length, 0, start)
 
 #define ZXN_ROM3_LD_BYTES   0x0556    // load/verify bytes from tape
 
 #define zxn_rom3_ld_bytes(type, loadNotVerify, length, start)		\
     zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_LD_BYTES, 0,			\
-                 (type << 8) | loadNotVerify, 0, length, 0, start)
+                 ((type) << 8) | loadNotVerify, 0, length, 0, start)
 
 #define ZXN_ROM3_CLS        0x0d6b    // CLS (for layer 0)
 
@@ -43,15 +47,55 @@ extern long zxn_rom3call_long(void* cont, short callno, short hlprime,
     zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_CLS, 0,	\
 		 0, 0, 0, 0, 0)
 
+#define ZXN_ROM3_EDITOR     0x0f2c    // main line-editor loop
+
+#define zxn_rom3_editor()					\
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_EDITOR, 0,		\
+		 0, 0, 0, 0, 0)
+
+#define ZXN_ROM3_WAIT_KEY   0x15d4    // wait for input on current input
+
+#define zxn_rom3_wait_key()					\
+    zxn_rom3call(zxn_p3cont_a, ZXN_ROM3_WAIT_KEY, 0,		\
+		 0, 0, 0, 0, 0)
+
+#define ZXN_ROM3_WAIT_KEY1  0x15de    // wait for input on current input
+
+#define zxn_rom3_wait_key1()					\
+    zxn_rom3call(zxn_p3cont_a, ZXN_ROM3_WAIT_KEY, 0,		\
+		 0, 0, 0, 0, 0)
+
 #define ZXN_ROM3_PRINT_A_2  0x15f2    // print char to current stream
 
 #define ZXN_ROM3_CALL_SUB   0x15f7    // execute routine at (HL') (trashes DE')
 
-#define ZXN_ROM3_CHAN_OPEN  0x1601    // open channel to stream
+#define ZXN_ROM3_CHAN_OPEN  0x1601    // make stream in A current
+
+#define zxn_rom3_chan_open(streamid)				\
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_CHAN_OPEN, 0,	\
+		 (streamid) << 8, 0, 0, 0, 0)
 
 #define ZXN_ROM3_CALL_JUMP  0x162c    // execute routine at HL
 
-#define ZXN_ROM3_CLASS_01   0x1c1f    // class 01, variable to assign
+#define ZXN_ROM3_SET_MIN    0x16b0    // reset editing area (and SET_WORK)
+
+#define zxn_rom3_set_min()				\
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_SET_MIN, 0,	\
+		 0, 0, 0, 0, 0)
+
+#define ZXN_ROM3_SET_WORK   0x16bf    // zap the work space (and SET_STK)
+
+#define zxn_rom3_set_work()					\
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_SET_WORK, 0,		\
+		 0, 0, 0, 0, 0)
+
+#define ZXN_ROM3_SET_STK    0x16c5    // reset calculator stack
+
+#define zxn_rom3_set_stk()                                     \
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_SET_STK, 0,         \
+                 0, 0, 0, 0, 0)
+
+#define ZXN_ROM3_CLASS_01   0x1c1f    // determine the address of a variable
 
 #define ZXN_ROM3_OUT_SP_NO  0x192a    // print a digit helper (E=' ' or $FF)
 
@@ -65,13 +109,17 @@ extern long zxn_rom3call_long(void* cont, short callno, short hlprime,
     zxn_rom3call(zxn_p3cont_hl, ZXN_ROM3_TEST_ROOM, 0,	\
 		 0, space, 0, 0, 0)
 
-#define ZXN_ROM3_PR_STRING  0x203c    // print string DE,BC
+#define ZXN_ROM3_PR_STRING  0x203c    // print string (DE = string, BC = count)
+
+#define zxn_rom3_pr_string(addr,len)                          \
+    zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_PR_STRING, 0,      \
+		 0, len, ((short) (addr)), 0, 0)
 
 #define ZXN_ROM3_PLOT_SUB   0x22e5    // plot a point
 
 #define zxn_rom3_plot_sub(x,y)				\
     zxn_rom3call(zxn_p3cont_null, ZXN_ROM3_PLOT_SUB, 0,	\
-		 0, (y << 8) | x, 0, 0, 0)
+		 0, ((y) << 8) | x, 0, 0, 0)
 
 
 #define ZXN_ROM3_STK_STO_S  0x2ab2    // store string on calc stack
